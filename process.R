3f401859c0c48c268dc2ee631dafee7548abb990
mgreen <mgreen@bournemouth.ac.uk>
2024-03-13 19:55:33 +0000

    median split please

--- process.R
+++ process.R
@@ -1,80 +1,86 @@
-setwd("~/Downloads")
-install.packages("tidyverse")
-install.packages("tidyverse")
-install.packages("tidyverse")
-install.packages("tidyverse")
-install.packages("tidyverse")
 library(tidyverse)
-dd=read_csv("JASP data Attractiveness and gender influence on morality.csv")
-View(dd)
-?pivot_longer
-d2=dd %>% mutate(participant=seq_along(`participant gender`))
-d2=dd %>% mutate(participant=seq_along(`participant gender`)) %>% relocate(participant)
-View(d2)
-d2 %>% pivot_longer(cols=2:last_col())
-d2 %>% pivot_longer(cols=3:last_col())
-d3=d2 %>% pivot_longer(cols=3:last_col())
-View(d3)
-d3 <- d3 %>% mutate(attractiveness=str_split(name, pattern=" "))
-d3 <- d3 %>% mutate(attractiveness=str_split(name, pattern=" ")[1])
-str_split(d3$age)
-str_split(d3$name)
-str_split(d3$name, pattern= " ")
-str_split(d3$name, pattern= " ")[[1]]
-str_split(d3$name, pattern= " ")[[1]][1]
-d3 <- d3 %>% mutate(attractiveness=str_split(d3$name, pattern= " ")[[1]][1])
-View(d3)
-d3 <- d3 %>% mutate(attractiveness=ifelse(name[1]=="u", "unattractive")
-)
-d3 <- d3 %>% mutate(attractiveness=ifelse(name[1]=="u", "unattractive", "attractive")
-)
-View(d3)
-d3 <- d3 %>% mutate(attractiveness=ifelse(substr(name, 1 =="u"), "unattractive", "attractive"))
-substr(d3$name, 1, 1)
-d3 <- d3 %>% mutate(attractiveness=ifelse(substr(name, 1, 1) =="u"), "unattractive", "attractive"))
-d3 <- d3 %>% mutate(attractiveness=ifelse(substr(name, 1, 1) =="u"), "unattractive", "attractive")
-d3 <- d3 %>% mutate(attractiveness=ifelse(substr(name, 1, 1) =="u", "unattractive", "attractive"))
-View(d3)
-unique(d3$attractiveness)
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_split(name, " ")[[1]][1]="f", "female", "male"))
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_split(name, " ")[[1]][1]=="f", "female", "male"))
-View(d4)
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_split(name, " ")[[1]][2]=="f", "female", "male"))
-View(d4)
-str_split(d4$name, " ")
-str_split(d4$name, " ")[[1]]
-str_split(d4$name, " ")[[1000]]
-str_split(d4$name, " ")[[1000]][2]
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_split(d4$name, " ")[[]][2]=="f", "female", "male"))
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_split(name, " ")[[]][2]=="f", "female", "male"))
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_split(name, " ")[[1]][2]=="f", "female", "male"))
-View(d4)
-d4 <- d3 %>% mutate(stimulus_gender = str_detect(name, "female"))
-Vierw(d4)
-View(d4)
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_detect(name, "female"), "female"))
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_detect(name, "female"), "female", NA))
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_detect(name, "male"), "male", name))
-View(d4)
-d4 <- d3 %>%
-mutate(stimulus_gender =
-ifelse(
-str_detect(name, "male"), "male",
-ifelse(str_detect(name, "female"), "female", NA)))
-View(d4)
-d4 <- d3 %>%
-mutate(stimulus_gender =
-ifelse(
-str_detect(name, "male"), "male", "female"))
-View(d4)
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_detect(name, "male"), "male", "female"))
-View(d4)
-d4 <- d3 %>% mutate(stimulus_gender = ifelse(str_detect(name, "female"), "female", "male"))
-View(d4)
-d4 <- d4 %>% mutate(stimulus_gender = ifelse(str_detect(name, "F"), "female", stimulus_gender))
-View(d4)
-d4 <- d4 %>% mutate(scenario_present = ifelse(str_detect(name, "M"), "scenario_present", "rating"))
-View(d4)
-d5 = d4 %>% mutate(which_scenario = str_sub(name, -1, -1))
-View(d5)
-q()
+dd <- 
+  read_csv("Clean_Names_JASP_data_Attractiveness_and_gender_influence_on_morality.csv", show_col_types = F) %>% 
+  #select(-c(290,291,292,293,294,295,296,297,298)) %>% 
+  mutate(participant=seq_along(`participant gender`)) %>% 
+  relocate() %>% 
+  relocate(participant, `participant age`, `participant gender`, `morality judgment_SC0`)
+
+dd <- dd %>% pivot_longer(cols=5:290, names_to="identifier")
+
+dd$attractiveness=NA
+dd <- dd %>% mutate(attractiveness   = ifelse(substr(identifier, 1, 1) =="u", "unattractive", attractiveness))
+dd <- dd %>% mutate(attractiveness   = ifelse(substr(identifier, 1, 1) =="a", "attractive", attractiveness))
+
+dd$stimulus_gender = NA
+dd <- dd %>% mutate(stimulus_gender  = ifelse(str_detect(identifier, "F"), "female", stimulus_gender))
+dd <- dd %>% mutate(stimulus_gender  = ifelse(str_detect(identifier, " male"), "male", stimulus_gender))
+dd <- dd %>% mutate(stimulus_gender  = ifelse(str_detect(identifier, " female"), "female", stimulus_gender))
+
+# normalise gender entries
+dd <- dd %>% mutate(`participant gender`= str_to_lower(`participant gender`) %>% as_factor())
+dd <- dd %>% mutate(identifier = ifelse(str_detect(identifier, "enari"), str_to_lower(identifier), identifier))
+# factorise most strings
+dd <- dd %>% mutate(attractiveness = as_factor(attractiveness), stimulus_gender=as_factor(stimulus_gender))
+
+
+
+#dd <- dd %>% mutate(scenario_present = ifelse(str_detect(identifier, "M"), "scenario_present", "rating"))
+
+dd <- dd %>% mutate(which_morality=NA)
+dd <- dd %>% mutate(which_morality = ifelse(str_detect(identifier, "M_1"), "Morality 1", which_morality))
+dd <- dd %>% mutate(which_morality = ifelse(str_detect(identifier, "M_2"), "Morality 2", which_morality))
+dd <- dd %>% mutate(which_morality = ifelse(str_detect(identifier, "M_3"), "Morality 3", which_morality))
+dd <- dd %>% mutate(which_morality = ifelse(str_detect(identifier, "M_4"), "Morality 4", which_morality))
+dd <- dd %>% mutate(which_morality = ifelse(str_detect(identifier, "M_5"), "Morality 5", which_morality))
+dd <- dd %>% mutate(which_morality = ifelse(str_detect(identifier, "M_6"), "Morality 6", which_morality))
+dd <- dd %>% mutate(which_morality=as_factor(which_morality))
+
+#qq=tibble(identifier=names(dd),logic=names(dd) %>% str_detect("unattractive male")) %>% filter(logic==TRUE)
+
+
+# stimulus number
+dd$stim_num=NA
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female1"), 1, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female2"), 2, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female3"), 3, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female4"), 4, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female5"), 5, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female6"), 6, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female7"), 7, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female8"), 8, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female9"), 9, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female10"), 10, stim_num))
+
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F1"), 1, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F2"), 2, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F3"), 3, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F4"), 4, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F5"), 5, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F6"), 6, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F7"), 7, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F8"), 8, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F9"), 9, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "F10"), 10, stim_num))
+
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 1"), 1, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 2"), 2, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 3"), 3, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 4"), 4, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 5"), 5, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 6"), 6, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 7"), 7, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 8"), 8, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 9"), 9, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "female 10"), 10, stim_num))
+
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 1"), 1, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 2"), 2, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 3"), 3, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 4"), 4, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 5"), 5, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 6"), 6, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 7"), 7, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 8"), 8, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 9"), 9, stim_num))
+dd <- dd %>% mutate(stim_num = ifelse(str_detect(identifier, "male 10"), 10, stim_num))
